d0d9c79baba9cec543855e970b43058af8bcf1b1
Bluey <bluetechfire@gmail.com>
2021-02-16 18:38:09 -0600

    Completion to chapter 2 of tutorial
    Logic added to render a chunk's worth of blocks; this brings project in line with video 2, The First Chunk, of tutorial.

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -7,28 +7,102 @@ public class Chunk : MonoBehaviour
     public MeshRenderer meshRenderer;
     public MeshFilter meshFilter;
 
+    int vertexIndex = 0;
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
+    bool[,,] voxelMap = new bool[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
+
     // Start is called before the first frame update
     void Start()
     {
-        int vertexIndex = 0;
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-        List<Vector2> uvs = new List<Vector2>();
+        PopulateVoxelMap();
+        CreateMeshData();
+        CreateMesh();
+    }
 
-        for (int f = 0; f < 6; f++)
+    void PopulateVoxelMap()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
         {
-            for (int i = 0; i < 6; i++)
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
             {
-                int triangleIndex = VoxelData.voxelTriangles[f, i];
-                vertices.Add(VoxelData.voxelVertices[triangleIndex]);
-                triangles.Add(vertexIndex);
-
-                uvs.Add(VoxelData.voxelUVs[i]);
-
-                vertexIndex++;
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    voxelMap[x, y, z] = true;
+                }
             }
         }
+    }
 
+    void CreateMeshData()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    AddVoxelDataToChunk(new Vector3(x, y, z));
+                }
+            }
+        }
+    }
+
+    bool CheckVoxel(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        if (x < 0 || x > VoxelData.ChunkWidth - 1 ||
+            y < 0 || y > VoxelData.ChunkHeight - 1 ||
+            z < 0 || z > VoxelData.ChunkWidth - 1)
+            return false;
+
+        return voxelMap[x, y, z];
+    }
+
+    void AddVoxelDataToChunk(Vector3 pos)
+    {
+        for (int f = 0; f < 6; f++)
+        {
+            if (!CheckVoxel(pos + VoxelData.faceChecks[f]))
+            {
+                /*for (int i = 0; i < 6; i++)
+                {
+                    int triangleIndex = VoxelData.voxelTriangles[f, i];
+                    vertices.Add(VoxelData.voxelVertices[triangleIndex] + pos);
+                    triangles.Add(vertexIndex);
+
+                    uvs.Add(VoxelData.voxelUVs[i]);
+
+                    vertexIndex++;
+                }*/
+                vertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTriangles[f, 0]]);
+                vertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTriangles[f, 1]]);
+                vertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTriangles[f, 2]]);
+                vertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTriangles[f, 3]]);
+
+                uvs.Add(VoxelData.voxelUVs[0]);
+                uvs.Add(VoxelData.voxelUVs[1]);
+                uvs.Add(VoxelData.voxelUVs[2]);
+                uvs.Add(VoxelData.voxelUVs[3]);
+
+                triangles.Add(vertexIndex);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
+                vertexIndex += 4;
+            }
+        }
+    }
+
+    void CreateMesh()
+    {
         // Create a mesh using the vertex, triangle, and uv lists created earlier in the for loop.
         Mesh mesh = new Mesh();
         mesh.vertices = vertices.ToArray();
@@ -39,6 +113,5 @@ public class Chunk : MonoBehaviour
 
         meshFilter.mesh = mesh;
     }
-
 
 }
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class VoxelData
 {
+    public static readonly int ChunkWidth = 5;
+    public static readonly int ChunkHeight = 15;
+
     public static readonly Vector3[] voxelVertices = new Vector3[8]
     {
         new Vector3(0.0f,0.0f,0.0f),
@@ -16,24 +19,56 @@ public class VoxelData
         new Vector3(0.0f,1.0f,1.0f)
     };
 
-    public static readonly int[,] voxelTriangles = new int[6, 6]
+    public static readonly Vector3[] faceChecks = new Vector3[6]
     {
-        {0,3,1,1,3,2},  // Back Face
-        {5,6,4,4,6,7},  // Front Face
-        {3,7,2,2,7,6},  // Top Face
-        {1,5,0,0,5,4},  // Bottom Face
-        {4,7,0,0,7,3},  // Left Face
-        {1,2,5,5,2,6}   // Right Face
+        Vector3.back,
+        Vector3.forward,
+        Vector3.up,
+        Vector3.down,
+        Vector3.left,
+        Vector3.right
     };
 
-    public static readonly Vector2[] voxelUVs = new Vector2[6]
+    public static readonly int[,] voxelTriangles = new int[6, 4]
     {
-        new Vector2(0.0f, 0.0f),    // Bottom Left of texture (beginning of first triangle)
-        new Vector2(0.0f, 1.0f),    // Top Left
-        new Vector2(1.0f, 0.0f),    // Bottom Right
-        new Vector2(1.0f, 0.0f),    // Bottom Right (beginning of second triangle)
-        new Vector2(0.0f, 1.0f),    // Top Left
-        new Vector2(1.0f, 1.0f)     // Top Right
+        // Full lookup table for vertex indices. They follow a specific pattern in order to
+        // draw the two triangles of a face. The fourth and fifth are duplicates and can be
+        // safely removed, as long as the pattern is preserved.
+        //{0,3,1,1,3,2},  // Back Face
+        //{5,6,4,4,6,7},  // Front Face
+        //{3,7,2,2,7,6},  // Top Face
+        //{1,5,0,0,5,4},  // Bottom Face
+        //{4,7,0,0,7,3},  // Left Face
+        //{1,2,5,5,2,6}   // Right Face
+
+        {0,3,1,2},  // Back Face
+        {5,6,4,7},  // Front Face
+        {3,7,2,6},  // Top Face
+        {1,5,0,4},  // Bottom Face
+        {4,7,0,3},  // Left Face
+        {1,2,5,6}   // Right Face
+    };
+
+    public static readonly Vector2[] voxelUVs = new Vector2[4]
+    {
+        // Full lookup table for UV indices. They follow the same pattern as the triangle
+        // pattern above. As before, the fourth and fifth are duplicates and can be removed
+        // for performance.
+
+        // Textures are normalized - that is, 0 is the beginning and 1 is the end regardless
+        // of what size the texture is. (0,0) is bottom left, (1,1) is top right. These vector
+        // addresses correspond to the given vertex indices of a face's two triangles.
+        //new Vector2(0.0f, 0.0f),    // Bottom Left of texture (beginning of first triangle)
+        //new Vector2(0.0f, 1.0f),    // Top Left
+        //new Vector2(1.0f, 0.0f),    // Bottom Right
+        //new Vector2(1.0f, 0.0f),    // Bottom Right (beginning of second triangle)
+        //new Vector2(0.0f, 1.0f),    // Top Left
+        //new Vector2(1.0f, 1.0f)     // Top Right
+
+        new Vector2(0.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(1.0f, 1.0f)
     };
 
 }
